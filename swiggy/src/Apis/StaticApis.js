91eb6ad2f82e78fef99c4a494fb7fa4e140249ef
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-04-10 15:46:30 +0530

    add static api

---
+++ swiggy/src/Apis/StaticApis.js
@@ -0,0 +1,215 @@
+export const TOP_RESTURANT = [
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/55a0283a136a749fff6428c23a558f90",
+    title: "Burger King",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/16/e50df5c0-50b1-4704-833d-f87ff391537c_364939.jpg",
+    title: "15-20 MINS LunchBox - Meals and Thalis",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/eysnlm254vmzhcbxdtlo",
+    title: "The Biryani Life",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/20/a9696b7b-9126-43fc-bfd9-56d05e165aa3_801441.JPG",
+    title: "Makhani Darbar",
+    rating: " 4.4 .30-35 mins",
+    description: "Biryani,North Indian, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/31/8fe2813c-ae82-4625-bd11-b970fef4c962_364938.jpg",
+    title: "Bakingo",
+    rating: " 4.4 .30-35 mins",
+    description: "Bakery, Desserts, American, Model Town",
+  },
+];
+// *********************** Carousel Data ***********************//
+export const CAROUSEL_DATA = [
+  {
+    id: 1,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Pizzas.png",
+  },
+  {
+    id: 2,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Pizzas.png",
+  },
+  {
+    id: 3,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/17/58760e8e-324f-479e-88fa-31800120ea38_Rolls1.png",
+  },
+  {
+    id: 4,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Dosa.png",
+  },
+  {
+    id: 5,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Biryani.png",
+  },
+  {
+    id: 5,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Paratha.png",
+  },
+  {
+    id: 6,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2025/1/24/05a939eb-fd4e-4308-b989-d1c54f4421b3_northindian1.png",
+  },
+  {
+    id: 7,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/f1263395-5d4a-4775-95dc-80ab6f3bbd89_pasta.png",
+  },
+  {
+    id: 8,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_cake.png",
+  },
+  {
+    id: 9,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_Shake.png",
+  },
+  {
+    id: 9,
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_coffee.png",
+  },
+];
+// *********************** ALL RESTURANT Data ***********************//
+
+export const RESTURANT_DATA = [
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/tdvqugbqjsbbi8pfufl0",
+    title: "Rissole King",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/16/e50df5c0-50b1-4704-833d-f87ff391537c_364939.jpg",
+    title: "15-20 MINS LunchBox - Meals and Thalis",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/33d8a6d9975c640ca151d39906bb4062",
+    title: "Bakingo",
+    rating: " 4.4 .35-40 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/eysnlm254vmzhcbxdtlo",
+    title: "The Biryani Life",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5f4f8c6714a5c77b52c8b14240ecc9a6",
+    title: "The Food Factory",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/20/a9696b7b-9126-43fc-bfd9-56d05e165aa3_801441.JPG",
+    title: "Makhani Darbar",
+    rating: " 4.4 .30-35 mins",
+    description: "Biryani,North Indian, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/20/9442c3d9-fc33-4a33-982d-15df57b178cb_858806.jpg",
+    title: "Big Indian Rolls & Kebabs",
+    rating: " 4.4 .30-35 mins",
+    description: "Biryani,North Indian, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/31/8fe2813c-ae82-4625-bd11-b970fef4c962_364938.jpg",
+    title: "Bakingo",
+    rating: " 4.4 .30-35 mins",
+    description: "Bakery, Desserts, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/19/d03def27-f52d-4217-8303-be0ec69000e6_128820.jpg",
+    title: "P.D. Cafe",
+    rating: " 4.4 .30-35 mins",
+    description: "Pizza, Desserts, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/2/14/9700f4bd-901d-477d-8168-e1f81e3c7d23_1024485%20(1).jpg",
+    title: "kingo",
+    rating: " 4.4 .30-35 mins",
+    description: "Bakery, Desserts, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bwsfenhmky8ym3unok2o",
+    title: "Bgo",
+    rating: " 4.4 .30-35 mins",
+    description: "Bakery, Desserts, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/21/251364c6-bd05-4967-8463-00652da761aa_944667.jpg",
+    title: "Bakingo",
+    rating: " 4.4 .30-35 mins",
+    description: "Bakery, Desserts, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5b6bc8db0ee1852f668e21624f235601",
+    title: "Madras Caffe",
+    rating: " 4.4 .30-35 mins",
+    description: "Bakery, Desserts, American, Model Town",
+  },
+  ,
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/16/e50df5c0-50b1-4704-833d-f87ff391537c_364939.jpg",
+    title: "15-20 MINS LunchBox - Meals and Thalis",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/33d8a6d9975c640ca151d39906bb4062",
+    title: "Bakingo",
+    rating: " 4.4 .35-40 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/eysnlm254vmzhcbxdtlo",
+    title: "The Biryani Life",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5f4f8c6714a5c77b52c8b14240ecc9a6",
+    title: "The Food Factory",
+    rating: " 4.4 .30-35 mins",
+    description: "Burger, American, Model Town",
+  },
+  {
+    image:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/20/a9696b7b-9126-43fc-bfd9-56d05e165aa3_801441.JPG",
+    title: "Makhani Darbar",
+    rating: " 4.4 .30-35 mins",
+    description: "Biryani,North Indian, American, Model Town",
+  }
+];
