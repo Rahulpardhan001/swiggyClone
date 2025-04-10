5729f216fa295b83c5d39107cab37483184eae91
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-04-10 15:46:44 +0530

    add not found page

---
+++ swiggy/src/component/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='flex justify-center items-center'>
+     <h2 className='text-4xl text-gray-500 font-bold py-7'>
+      404 Not Found 
+     </h2>
+    </div>
+  )
+}
+
+export default NotFound
