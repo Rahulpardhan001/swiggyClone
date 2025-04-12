8ac6a736e238be5abbf7b6d852109561c6287a95
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-04-12 12:48:52 +0530

    responsive footer

--- swiggy/src/component/Footer.jsx
+++ swiggy/src/component/Footer.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IMAGES } from "../assets/ImageGallary/Image";
 import { FaLinkedin, FaTwitter, FaPinterest } from "react-icons/fa6";
 import { FaInstagram, FaFacebookF } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="px-24  py-9 bg-[#F0F0F5]">
+      <footer className="max-w-7xl mx-auto  px-4 py-3">
+       
       {/* Download App Section */}
-      <div className="flex flex-col md:flex-row justify-center items-center gap-6 py-6 text-center md:text-left px-4">
-        <h1 className="text-lg md:text-xl w-full md:w-[50%]  font-extrabold ">
+      <div className="flex flex-col md:flex-row px-3 items-center gap-6 py-6 text-center md:text-left ">
+        <h1 className="text-sm md:text-xl w-full md:w-[50%] font-extrabold line-clamp-2 mx-auto">
           For better experience, download the Swiggy app now
         </h1>
-        <div className="flex flex-col md:flex-row gap-4">
-          <div className="w-36 md:w-40">
-            <img className="w-full" src={IMAGES.PLAY_STORE} alt="PLAYSTORE" />
+        <div className="flex flex-col  justify-end gap-7 mx-auto   md:flex-row">
+          <div className="w-36 md:w-40 ">
+            <img className="w-full " src={IMAGES.PLAY_STORE} alt="PLAYSTORE" />
           </div>
           <div className="w-36 md:w-40">
             <img className="w-full" src={IMAGES.APP_STORE} alt="APPSTORE" />
@@ -22,15 +23,15 @@ function Footer() {
       </div>
 
       {/* Bottom Section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-10 px-6 md:px-16 py-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-10  py-10 ">
 
         {/* Logo & Copyright */}
         {/* <div className="flex flex-col  items-start col-span-3 lg:col-span-1 gap-2 md:gap-4"> */}
-        <div className="flex flex-col items-start sm:col-span-2 lg:col-span-1 gap-2 md:gap-4">
+        <div className="flex flex-col items-start sm:col-span-2 lg:col-span-1 gap-1 md:gap-2 mx-auto">
 
-            <div className="flex items-center">
-            <div className="w-16 ">
-            <img className="w-full" src={IMAGES.SWIGGY_LOGOS} alt="Swiggy Logo" />
+            <div className="flex flex-col md:flex-row items-center  w-full">
+            <div className="w-16 h-16 mx-auto ">
+            <img className="w-full  " src={IMAGES.SWIGGY_LOGOS} alt="Swiggy Logo" />
           </div>
             <h2 className="text-2xl font-bold text-[#ff5200] ">Swiggy</h2>
             </div>
@@ -38,7 +39,8 @@ function Footer() {
         </div>
 
         {/* Company */}
-        <div>
+        <div className="mx-auto">
+
           <h2 className="text-lg font-bold mb-4">Company</h2>
           <ul className="text-gray-600 flex flex-col gap-4 text-sm">
             <li>About Us</li>
@@ -55,7 +57,7 @@ function Footer() {
         </div>
 
         {/* Contact & Legal */}
-        <div>
+        <div className="mx-auto">
           <h2 className="text-lg font-bold mb-4">Contact</h2>
           <ul className="text-gray-600 flex flex-col gap-4 text-sm mb-8">
             <li>Help & Support</li>
@@ -73,7 +75,7 @@ function Footer() {
         </div>
 
         {/* Available in */}
-        <div>
+        <div className="mx-auto">
           <h2 className="text-lg font-bold mb-4">Available in:</h2>
           <ul className="text-gray-600 flex flex-col gap-4 text-sm">
             <li>Bangalore</li>
@@ -86,7 +88,7 @@ function Footer() {
         </div>
 
         {/* Life at Swiggy + Social Links */}
-        <div>
+        <div className="mx-auto">
           <h2 className="text-lg font-bold mb-4">Life at Swiggy</h2>
           <ul className="text-gray-600 flex flex-col gap-4 text-sm mb-8">
             <li>Explore with Swiggy</li>
@@ -95,7 +97,7 @@ function Footer() {
           </ul>
 
           <h2 className="text-lg font-bold mb-4">Social Links</h2>
-          <div className="flex gap-4 text-gray-700 text-xl">
+          <div className="flex flex-col md:flex-row gap-4 text-gray-700 text-xl">
             <a href="/"><FaLinkedin /></a>
             <a href="/"><FaInstagram /></a>
             <a href="/"><FaFacebookF /></a>
@@ -109,3 +111,4 @@ function Footer() {
 }
 
 export default Footer;
+
